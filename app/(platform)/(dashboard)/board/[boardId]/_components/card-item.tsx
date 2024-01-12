860962adbbc62d27d8a4a8a52577255e6f7f7743
jayzernguyenlc <ngocminhpham2004hn@gmail.com>
2024-01-12 10:29:42 +0700

    card modal

--- app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
@@ -3,6 +3,7 @@
 import React from "react";
 import { Card } from "@prisma/client";
 import { Draggable } from "@hello-pangea/dnd";
+import { useCardModal } from "@/hooks/use-card-modal";
 
 interface CardItemProps {
   data: Card;
@@ -10,6 +11,8 @@ interface CardItemProps {
 }
 
 function CardItem({ data, index }: CardItemProps) {
+const cardModal = useCardModal()
+
   return (
     <Draggable draggableId={data.id} index={index}>
       {(provided) => (
@@ -19,6 +22,7 @@ function CardItem({ data, index }: CardItemProps) {
           ref={provided.innerRef}
           role="button"
           className="truncate border-2 border-transparent hover:border-black py-2 px-3 text-sm bg-white rounded-md shadow-sm"
+          onClick={() => cardModal.onOpen(data.id)}
         >
           {data.title}
         </div>
